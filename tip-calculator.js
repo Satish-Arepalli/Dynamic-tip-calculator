56044fa211eb1654c16ef946f3b19a5f5317ad48
Satish-Arepalli <149222682+Satish-Arepalli@users.noreply.github.com>
2024-04-23 12:04:49 +0530

    Add files via upload

---
+++ tip-calculator.js
@@ -0,0 +1,25 @@
+let billAmountInput = document.getElementById("billAmount");
+let percentageTipInput = document.getElementById("percentageTip");
+let tipAmountInput = document.getElementById("tipAmount");
+let totalInput = document.getElementById("totalAmount");
+let errorMessageElement = document.getElementById("errorMessage");
+
+function calculateTip() {
+    let billAmountInputValue = billAmountInput.value;
+    let percentageTipInputValue = percentageTipInput.value;
+
+    if (billAmountInputValue === "") {
+        errorMessageElement.textContent = "Please Enter a Valid Input";
+    } else if (percentageTipInputValue === "") {
+        errorMessageElement.textContent = "Please Enter a Valid Input";
+    } else {
+        errorMessageElement.textContent = "";
+        let billAmount = parseInt(billAmountInputValue);
+        let percentageTip = parseInt(percentageTipInputValue);
+
+        let calculatedTip = (percentageTip / 100) * billAmount;
+        let totalAmount = billAmount + calculatedTip;
+        tipAmountInput.value = calculatedTip;
+        totalInput.value = totalAmount;
+    }
+}
